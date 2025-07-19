428cf6cc8db316db0627cb100e682aaf99ca4b0c
Gabriel Mello <63933270+Mello-Gabriel@users.noreply.github.com>
2025-07-19 16:42:05 -0300

    Applying css through static files

--- my_site/members/models.py
+++ my_site/members/models.py
@@ -1,24 +1,41 @@
-from django.db import models
-from django.core.exceptions import ValidationError
-import phonenumbers
+"""Module to define the Member model for the members app."""
 
-def validate_international_phone_number(value):
-    """
-    Validates that the phone number is a valid international number.
-    """
+import phonenumbers
+from django.core.exceptions import ValidationError
+from django.db import models
+
+
+def validate_international_phone_number(value: str) -> None:
+    """Validate if the phone number is a valid international number."""
     try:
         parsed_number = phonenumbers.parse(value, "BR")
         if not phonenumbers.is_valid_number(parsed_number):
-            raise ValidationError("Please enter a valid international phone number (e.g., +5531999999999).")
-    except phonenumbers.phonenumberutil.NumberParseException as e:
-        raise ValidationError(f"Invalid phone number format: {e}")
+            message = (
+                "The phone number is not valid. "
+                "Please enter a valid international phone number."
+                "Example: +5511999999999 (Brazil)"
+            )
+            raise ValidationError(message)
+    except phonenumbers.phonenumberutil.NumberParseException as err:
+        e = f"The phone number '{value}' raised an error: {err}"
+        raise ValidationError(e) from err
+
 
 class Member(models.Model):
-  firstname = models.CharField(max_length=255)
-  lastname = models.CharField(max_length=255)
-  phone = models.CharField(max_length=20, null=True, blank=True, validators=[validate_international_phone_number])
-  joined_date = models.DateField(null=True, blank=True)
-  email = models.EmailField(null=True, blank=True)
+    """Model to represent a member of the members app."""
 
-  def __str__(self):
-    return f"{self.firstname} {self.lastname}"
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phone = models.CharField(
+        max_length=20,
+        default=None,
+        null=False,
+        blank=False,
+        validators=[validate_international_phone_number],
+    )
+    joined_date = models.DateField(default=None)
+    email = models.EmailField(default=None)
+
+    def __str__(self) -> str:
+        """Return a string representation of the member."""
+        return f"{self.firstname} {self.lastname}"
--- my_site/members/views.py
+++ my_site/members/views.py
@@ -1,55 +1,59 @@
-from django.http import HttpResponse
-from django.template import loader
-from django.shortcuts import render, redirect, get_object_or_404
 from datetime import date
 
-from .models import Member
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.template import loader
+
 from .forms import MemberForm
+from .models import Member
 
 
 def members(request):
-  mymembers = Member.objects.all().values()
-  template = loader.get_template('members/all_members.html')
-  context = {
-    'mymembers': mymembers,
-  }
-  return HttpResponse(template.render(context, request))
-  
+    mymembers = Member.objects.all().values()
+    template = loader.get_template("members/all_members.html")
+    context = {"mymembers": mymembers}
+    total_members = mymembers.count()
+    return HttpResponse(template.render(context, request))
+
+
 def details(request, id):
-  mymember = Member.objects.get(id=id)
-  template = loader.get_template('members/details.html')
-  context = {
-    'mymember': mymember,
-  }
-  return HttpResponse(template.render(context, request))
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template("members/details.html")
+    context = {
+        "mymember": mymember,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def main(request):
     template = loader.get_template("members/main.html")
     return HttpResponse(template.render({}, request))
 
+
 def add_member(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = MemberForm(request.POST)
         if form.is_valid():
             member = form.save(commit=False)
             member.joined_date = date.today()
             member.save()
-            return redirect('members')  # Redireciona para a lista de membros após adicionar
+            return redirect(
+                "members"
+            )  # Redireciona para a lista de membros após adicionar
     else:
         form = MemberForm()
-    return render(request, 'members/add_member.html', {'form': form})
+    return render(request, "members/add_member.html", {"form": form})
+
 
 def edit_member(request, id):
     member = get_object_or_404(Member, pk=id)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = MemberForm(request.POST, instance=member)
         if form.is_valid():
             form.save()
-            return redirect('details', id=member.id)
+            return redirect("details", id=member.id)
     else:
         form = MemberForm(instance=member)
 
-    return render(request, 'members/add_member.html', {'form': form, 'member': member})
-    
+    return render(request, "members/add_member.html", {"form": form, "member": member})
